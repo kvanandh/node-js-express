65182e40b3d6436793ecf2b005cbdac0f03693cd
kvanandh <kvanandh@yahoo.com>
2021-12-08 16:50:40 +0530

    app.js modified to add another route

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const port = 4000
 app.get('/',(req,res)=>{
     res.send('Hello Everyone')
 })
-
+app.get('/about',(req,res)=>{
+    res.send('This is Hello')
+})
 app.listen(port,()=>{
     console.log(`Example app listening at http://localhost:${port}`)
 })
